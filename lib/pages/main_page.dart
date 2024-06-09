e4a6a4d6fd995b42b21c1a6186927ed0142eb34f
Sandipshrestha1 <sandipshrestha2050@gmail.com>
2024-06-09 10:49:11 +0545

    cupertinoTabScaffold and CupertinoTabBar Widgets

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,5 +1,6 @@
+import 'package:bmi_app/pages/bmi_page.dart';
+import 'package:bmi_app/pages/history_page.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -11,15 +12,36 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  final List<Widget> _tabs = [
+    BMIPage(),
+    HistoryPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
       navigationBar: CupertinoNavigationBar(
         middle: Text("IBMI"),
       ),
-      child: Container(
-          // color: Colors.red,
-          ),
+      child: CupertinoTabScaffold(
+        tabBar: CupertinoTabBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.home),
+              label: "BMI",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.person),
+              label: "History",
+            ),
+          ],
+        ),
+        tabBuilder: (context, index) {
+          return CupertinoTabView(builder: (context) {
+            return _tabs[index];
+          });
+        },
+      ),
     );
   }
 }
